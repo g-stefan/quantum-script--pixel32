0fa0dbe40a54dec51da9330cd10ff32fd501cf2c
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:21:32 +0300

    update

--- source/quantum-script-extension-pixel32-version.cpp
+++ source/quantum-script-extension-pixel32-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace Pixel32 {
 				namespace Version {
 
-					static const char *version_ = "1.6.0";
-					static const char *build_ = "6";
-					static const char *versionWithBuild_ = "1.6.0.6";
-					static const char *datetime_ = "2021-03-15 12:30:23";
+					static const char *version_ = "1.7.0";
+					static const char *build_ = "7";
+					static const char *versionWithBuild_ = "1.7.0.7";
+					static const char *datetime_ = "2021-07-06 15:11:50";
 
 					const char *version() {
 						return version_;
