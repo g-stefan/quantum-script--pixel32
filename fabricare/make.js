d9a18685f8a372cc93a62546ebd7a30569465dbe
Grigore Stefan <g_stefan@yahoo.com>
2022-12-13 13:22:00 +0200

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -3,11 +3,13 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
+messageAction("make");
+
 runInPath("source/XYO/QuantumScript.Extension/Pixel32",function(){
 	exitIf(fileToCS("--touch=Library.cpp","--file-in=Library.js","--file-out=Library.Source.cpp","--is-string","--name=librarySource"));
 });
 
-if(!Fabricare.include("msvc.make."+Project.make)){
+if(!Fabricare.include("make."+Project.make)){
 	messageError("Don't know how to make '"+Project.make+"'!");
 	exit(1);
 };
